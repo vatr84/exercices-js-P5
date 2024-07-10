f339c7c1e268f3a41d1d2363a3824890694db9d7
vatr84 <vatr@hotmail.fr>
2024-07-10 17:50:44 +0200

    Exercice9

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,13 @@
 // votre code ici
-
+function checkAge(age) {
+    let msg;
+    if (age < 18) {
+        msg = "mineur";
+    } else if (age <= 65) {
+        msg = "majeur";
+    } else {
+        msg = "senior";
+    }
+    console.log("Vous êtes " + msg);
+}
 export default checkAge;
